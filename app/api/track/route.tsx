5f77d783e09d67fb4770f0079c9e53d3b7cd8063
HarshDobariya10 <dobariyaharsh93@gmail.com>
2026-01-08 21:32:17 +0530

    Implement tracking logic

---
+++ app/api/track/route.tsx
@@ -0,0 +1,67 @@
+import { db } from "@/configs/db";
+import { pageViewTable } from "@/configs/schema";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import {UAParser} from 'ua-parser-js';
+
+export async function POST(req:NextRequest){
+    const body = await req.json();
+
+    //fetch all required data from Analytics.js
+    const parser = new UAParser(req.headers.get('user-agent') || '');  // get all info about devics(mobile, crom, desktop..
+    const deviceInfo = parser.getDevice()?.model;
+    const osInfo = parser.getOS()?.name;
+    const browserInfo = parser.getBrowser()?.name;
+    const ip=req.headers.get('x-forwarded-for')?.split(',')[0] 
+            || req.headers.get('get-real-ip') 
+            || '71.71.22.54'; //Fallback IP for testing
+
+    const geoRes= await fetch(`http://ip-api.com/json/71.71.22.54`);
+    // const geoRes= await fetch(`http://ip-api.com/json/${ip}`);
+    const geoInfo= await geoRes.json();
+
+    console.log("Body Data:", body);
+    console.log("Device Info:", deviceInfo);
+    console.log("OS Info:", osInfo);
+    console.log("Browser Info:", browserInfo);
+    console.log("IP Address:", ip);
+    console.log("Geo Info:", geoInfo);
+
+
+    //Insert to DB
+    let result;
+    if(body?.type=='entry'){
+        result = await db.insert(pageViewTable).values({
+            visitorId: body.visitorId,
+            websiteId: body.websiteId,
+            domain: body.domain,
+            url: body.url,
+            type: body.type,
+            referrer: body.referrer,
+            entryTime: body.entryTime,
+            exitTime: body.exitTime,
+            totalActiveTime: body.totalActiveTime,
+            urlParams: body.urlParams,
+            utm_sorce: body.utmSource,
+            utm_medium: body.utmMedium,
+            utm_campaign: body.utmCampaign,
+            device: deviceInfo,
+            os: osInfo,
+            browser: browserInfo,
+            city: geoInfo.city,
+            region: geoInfo.regionName,
+            country: geoInfo.country,
+            ipAddress: ip || '',
+            RefParams: body.RefParams,
+        }).returning();
+    }else{
+        await db.update(pageViewTable).set({
+            exitTime: body.exitTime,
+            totalActiveTime: body.totalActiveTime,
+        }).where(eq(pageViewTable.visitorId, body.visitorId)).returning();
+    }
+
+    console.log("Insert Result:", result);
+
+    return NextResponse.json({ message: "Data received successfully", data: result });
+}
---
+++ public/analytics.js
@@ -0,0 +1,79 @@
+(function(){
+    console.log("Analytics script loaded.");
+
+    function generateUUID(){   
+        return Date.now().toString(36) + Math.random().toString(36).substr(2, 9);
+    }
+    let visitorId = localStorage.getItem('webtrack_visitor_id');  // this is for avoiding multitab time tracking
+    if(!visitorId){
+        visitorId = generateUUID();
+        localStorage.setItem('webtrack_visitor_id', visitorId);
+    }
+
+    const script=document.currentScript;
+    const websiteId=script.getAttribute("data-website-id");
+    const domain=script.getAttribute("data-domain");
+
+    const entryTime = Date.now(); // get entry time
+    const referrer=document.referrer || 'Direct'; // get referrer
+
+    // utm sources
+    const urlParams=new URLSearchParams(window.location.search);
+    const utmSource=urlParams.get('utm_source') || '';
+    const utmMedium=urlParams.get('utm_medium') || '';
+    const utmCampaign=urlParams.get('utm_campaign') || '';
+    const RefParams=window.location.href.split('?')[1] || '';
+
+    const data={
+        type: 'entry',
+        websiteId: websiteId,
+        domain: domain,
+        entryTime: entryTime,
+        referrer: referrer,
+        url: window.location.href,
+        visitorId: visitorId,
+        urlParams,
+        utmSource,
+        utmMedium,
+        utmCampaign,
+        RefParams
+    }
+
+    fetch('http://localhost:3000/api/track', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+
+    // Active time tracking ------------------------------------
+    let activeTime = Date.now();
+    let totalActiveTime = 0;
+
+    const handleExit = () => {
+        const exitTime = Date.now();
+        totalActiveTime += Date.now() - activeTime;
+        fetch('http://localhost:3000/api/track', {
+            method: 'POST',
+            keepalive: true,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                type: 'exit',
+                websiteId: websiteId,
+                domain: domain,
+                exitTime: exitTime,
+                totalActiveTime: totalActiveTime,
+                visitorId: visitorId,
+            })
+        })
+        localStorage.clear(); // clear visitor ID on exit
+    }
+
+    window.addEventListener('beforeunload', handleExit);
+    // window.addEventListener('pagehide', handleExit);
+
+})();
